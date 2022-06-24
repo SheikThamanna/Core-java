40d0f833f3062c90f0a6dfb34284ff250690d541
SheikThamanna <sheikthamanna15@gmail.com>
2022-06-24 15:46:58 +0530

    ok

--- ThamannaApp/src/EqualityTest.java
+++ ThamannaApp/src/EqualityTest.java
@@ -2,7 +2,7 @@
 public class EqualityTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		int ₹ = 80000; //₹ represents Indian Rupee symbol
         String msg = (₹ >= 50000) ? "Good bonus" : "Average bonus";
 		System.out.println(msg);
