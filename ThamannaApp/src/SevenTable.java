8e36587b3f1434cfa04e10f25baf0505e0a8b657
SheikThamanna <sheikthamanna15@gmail.com>
2022-06-27 13:22:31 +0530

    ok

---
+++ ThamannaApp/src/SevenTable.java
@@ -0,0 +1,15 @@
+
+public class SevenTable {
+
+	public static void main(String[] args) {
+     int tableOf=7;
+     int[]arr= {1,2,3,4,5,6,7,8,9,10};
+     //for(int i=0;i<arr.length;i++) {
+    for(int x:arr) {
+    	 System.out.println(tableOf +"*" + x+ "=" +x*tableOf);
+    	 
+     }
+     
+	}
+
+}
