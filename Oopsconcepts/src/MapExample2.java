fa9780c95e48525fa63ba517b50288dba44d3e3d
SheikThamanna <sheikthamanna15@gmail.com>
2022-07-01 16:52:16 +0530

    ok

---
+++ Oopsconcepts/src/MapExample2.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Collection;
+
+public class MapExample2 {
+public static void main(String[] args) {
+		
+		HashMap<String,String> MyDetails=new HashMap<String,String>();
+	     System.out.println(MyDetails);
+	     MyDetails.put("FirstName","Alex");
+	     MyDetails.put("LastName","john");
+	     
+	     MyDetails.put("SureName","john");
+	     MyDetails.put("PhoneNumber","9008814696");
+	     MyDetails.put("PassWord","##99&&");
+	     MyDetails.put("DOB","20/04/1976");
+	     MyDetails.put("Gender","Female");
+	     Collection<String> values=MyDetails.values();
+	     for(String value:values) {
+	    	 System.out.println(value);
+	     }
+
+}
+}
