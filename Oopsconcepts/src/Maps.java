c410e2be42721fddfbbaa6e196760425c6b71cbc
SheikThamanna <sheikthamanna15@gmail.com>
2022-07-01 16:03:42 +0530

    ok

---
+++ Oopsconcepts/src/Maps.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+public class Maps {
+	public static void main(String[] args) {
+		HashMap MyDetails=new HashMap();
+	     System.out.println(MyDetails);
+	     MyDetails.put("Firstname","Alex");
+	     MyDetails.put("Firstname","john");
+	     
+	     MyDetails.put("SureName","john");
+	     MyDetails.put("PhoneNumber",9008814696L);
+	     MyDetails.put("PassWord","##99&&");
+	     MyDetails.put("DOB","20/04/1976");
+	     MyDetails.put("Gender","Female");
+	     
+	     System.out.println(MyDetails);
+
+		
+	}
+	
+
+}
