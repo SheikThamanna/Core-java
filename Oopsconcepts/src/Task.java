d027bed48b5414cdc57ad1544ee7a759230d6af6
SheikThamanna <sheikthamanna15@gmail.com>
2022-07-05 10:18:31 +0530

    ok

---
+++ Oopsconcepts/src/Task.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Task {
+	public static void main(String[] args) {
+	ArrayList <String> al=new ArrayList<String>();
+	al.add("1");
+	al.add("2");
+	al.add("3");
+	al.add("4");
+	System.out.println(al);
+	int a1=Integer.parseInt("1");
+	int b1=Integer.parseInt("2");
+	int c1=Integer.parseInt("3");
+	int d1=Integer.parseInt("4");
+	int result=((a1+b1)*(c1+d1));
+	System.out.println(result);
+	
+	}
+
+}
